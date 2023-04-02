39fd5116ed6356d7db1d85f6f1205d7b9a5408a0
Aika <m.aijamal07@icloud.com>
2023-04-02 12:39:42 -0500

    new

---
+++ src/MapTask.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapTask {
+
+    public static void main(String[] args) {
+
+        String str = "kkkubaaa";
+
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            if (map.containsKey(str.charAt(i))){
+                map.put(str.charAt(i), map.get(str.charAt(i))+1);
+            }else {
+                map.put(str.charAt(i), 1);
+            }
+        }
+
+        System.out.println(map);
+
+
+    }
+}
